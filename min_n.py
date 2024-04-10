91b11cadb514bff2588b6835bb497af88c12b0e5
mr-anton-icg <148938357+mr-anton-icg@users.noreply.github.com>
2024-04-10 08:32:52 +0000

    add second min

--- min_n.py
+++ min_n.py
@@ -5,8 +5,27 @@ def min_n_variables(elements):
             min_v = x
     return min_v
 
+# # elements = [6**6, 7**5, 5**7]
+# elements = [5,1,9,0,7]
+# assert min_n_variables(elements) == 0
+# print('min value of elements=', min_n_variables(elements))
+
+def second_min_n_variables(elements):
+    min_v = elements[0]
+    for x in elements:
+        if x < min_v:
+            min_v = x
+
+    second_min_v = elements[0]
+    for x in elements:
+        if x < second_min_v:
+            if x > min_v:
+                second_min_v = x
+                
+    return second_min_v
+
 # elements = [6**6, 7**5, 5**7]
 elements = [5,1,9,0,7]
-assert min_n_variables(elements) == 0
-print('min value of elements=', min_n_variables(elements))
+assert second_min_n_variables(elements) == 1
+print('second min value of elements=', second_min_n_variables(elements))
 
