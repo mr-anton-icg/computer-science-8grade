9a987fe761b45def5b560febf7ef496989ff1eeb
mr-anton-icg <148938357+mr-anton-icg@users.noreply.github.com>
2024-05-15 06:27:34 +0000

    add changes

---
+++ min_3.py
@@ -0,0 +1,4 @@
+a,b,c = 1,2,3
+
+if a<b<c:
+    print('min a=', a)
--- min_n.py
+++ min_n.py
@@ -28,4 +28,3 @@ def second_min_n_variables(elements):
 elements = [5,1,9,0,7]
 assert second_min_n_variables(elements) == 1
 print('second min value of elements=', second_min_n_variables(elements))
-
